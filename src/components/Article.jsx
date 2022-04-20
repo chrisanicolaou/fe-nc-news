928bc8a4a9fe3b0fb78b97b4d1ecbbe900e927d6
Christos Nicolaou <chrisanicolaou>
2022-04-20 15:43:47 +0100

    Starts setup for ticket 8

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -55,7 +55,7 @@ const Article = () => {
           );
         })}
       </ul>
-      <Comment />
+      <Comment article={article} />
     </article>
   );
 };
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,8 @@
-const Comment = () => {
+import { useState } from "react";
+
+const Comment = ({ article }) => {
+  const [username, setUsername] = useState("Xx_c0d3m45t3r_xX");
+  const [newCommentToPost, setNewCommentToPost] = useState("");
   return <h1>Post Comment Logic</h1>;
 };
 
