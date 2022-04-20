c145b163dd938c30b9e41abdd5c1ce8f20cbc499
Christos Nicolaou <chrisanicolaou>
2022-04-20 12:40:53 +0100

    Sets up API call for Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getReq } from "../utils/api";
 import Comment from "./Comment";
 
 const Article = () => {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState([]);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    const asyncEffect = async () => {
+      try {
+        const result = await getReq(`/articles/${article_id}`);
+        setArticle(result);
+        console.log(result);
+      } catch (err) {
+        setErr(
+          "404 - We don't know where your article is! Please try again :)"
+        );
+      }
+    };
+    asyncEffect();
+  }, []);
+
+  if (err) {
+    return <p>{err}</p>;
+  }
+
   return (
-    <>
-      <h1>Individual Article Logic</h1>
+    <article className="text-center">
+      <h1>Individual Article {article_id} Logic</h1>
       <Comment />
-    </>
+    </article>
   );
 };
 
