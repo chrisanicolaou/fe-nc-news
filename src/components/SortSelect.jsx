730af4678a1ac8cce639b155f5c43dff04d8cb45
Christos Nicolaou <chrisanicolaou>
2022-04-22 09:45:34 +0100

    Changes category names for user clarity

--- src/components/SortSelect.jsx
+++ src/components/SortSelect.jsx
@@ -5,8 +5,8 @@ const options = [
   { value: "sort_by=created_at&order=asc", label: "Oldest" },
   { value: "sort_by=comment_count&order=desc", label: "Most Popular" },
   { value: "sort_by=comment_count&order=asc", label: "Least Popular" },
-  { value: "sort_by=votes&order=desc", label: "Hottest" },
-  { value: "sort_by=votes&order=asc", label: "Coldest" },
+  { value: "sort_by=votes&order=desc", label: "Highest Votes" },
+  { value: "sort_by=votes&order=asc", label: "Lowest Votes" },
 ];
 
 const SortSelect = ({ setSelectedOption }) => {
